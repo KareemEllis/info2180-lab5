f153092d838475e32a03f642eb6807e26643721a
Kareem Ellis <kareemellis55@gmail.com>
2022-11-11 11:43:18 -0500

    Outputed results to index.html as table

--- world.php
+++ world.php
@@ -4,11 +4,27 @@ $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
 
-$conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 // $stmt = $conn->query("SELECT * FROM countries");
 // $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-
+function displayTable($results){
+  echo(
+    "<table>".
+      "<tr>".
+        "<th>Name</th> <th>Continent</th> <th>Independence</th> <th>Head of State</th>"
+  );
+  
+  foreach ($results as $row){
+    echo "<tr>";
+    echo "<td>".$row['name']."</td>" . "<td>".$row['continent']."</td>" . "<td>".$row['independence_year']."</td>" . "<td>".$row['head_of_state']."</td>";
+    echo "</tr>";
+  }
+  
+  echo(
+      "</tr>".
+    "</table>"
+  );
+}
 
 
 if($_SERVER['REQUEST_METHOD'] == 'GET'){
@@ -17,28 +33,23 @@ if($_SERVER['REQUEST_METHOD'] == 'GET'){
 
   if(empty($userInput)){
     //IF INPUT IS EMPTY THEN SEARCH FOR ALL COUNTRIES
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
     $stmt = $conn->query("SELECT * FROM countries");
     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    echo "<ul>";
-    foreach ($results as $row){
-      echo "<li>".$row['name'] . ' is ruled by ' . $row['head_of_state']. "</li>";
-    }
-    echo "</ul>";
+    displayTable($results);
   }
 
   else{
     //SEARCH FOR ENTERED COUNTRY
-    $stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE '%:userInput%'");
-    $stmt->bindParam(':userInput', $userInput, PDO::PARAM_STR);
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE :userInput");
+    $stmt->bindValue(':userInput', "%$userInput%", PDO::PARAM_STR);
+    $stmt->execute();
     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    foreach ($results as $row){
-      echo "<li>".$row['name'] . ' is ruled by ' . $row['head_of_state']. "</li>";
-    }
+    displayTable($results);
 
-    //NEXT TO DO:
-    //MAKE IT SO THAT IF A COUNTRY IS NOT FOUND, WE PRINT OUT A MESSAGE INDICATING SO
   }
 
 }
